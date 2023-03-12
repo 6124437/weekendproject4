de573a366eba8e839721760ccaf6ca121826c123
Kerem Bakir <kerembakir@gmail.com>
2023-03-12 13:39:27 +0100

    Add configservice

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,10 +14,10 @@ async function bootstrap() {
   app.enableCors(corsOptions);
 
   const config = new DocumentBuilder()
-    .setTitle('Cats example')
-    .setDescription('The cats API description')
+    .setTitle('Ballot API')
+    .setDescription('The Ballot API')
     .setVersion('1.0')
-    .addTag('cats')
+    .addTag('ballot')
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
